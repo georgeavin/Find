e661c8b4791aee1c7e0e6ea1d8f55ed562a0d902
georgeavin <jgeorgeavinash@gmail.com>
2022-03-16 12:17:08 +0530

    New Class

---
+++ src/org/kil/Kiling.java
@@ -0,0 +1,29 @@
+package org.kil;
+
+
+
+public class  Kiling{
+
+	private void empName() {
+		System.out.println("Employee Name : Raj");
+	}
+	
+	private void empId() {
+		System.out.println("Employee Id : 1234");
+	}
+	
+	private void empVin() {
+		System.out.println("Employee Vin");
+	}
+
+	public static void main(String[] args) {
+		
+		Kiling k=new Kiling();
+		
+		k.empId();
+		k.empName();
+		k.empVin();
+		
+	}
+
+}

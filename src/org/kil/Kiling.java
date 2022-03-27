c6620b91fe6fab7d9d7a0905671c89beabdeb8a4
georgeavin <jgeorgeavinash@gmail.com>
2022-03-27 21:57:50 +0530

    Class Added

--- src/org/kil/Kiling.java
+++ src/org/kil/Kiling.java
@@ -11,6 +11,10 @@ public class  Kiling{
 		System.out.println("Employee Id : 1234");
 	}
 	
+	private void empNew() {
+		System.out.println("New Employee pin : 56245");
+	}
+	
 	private void empVin() {
 		System.out.println("Employee Vin");
 	}
